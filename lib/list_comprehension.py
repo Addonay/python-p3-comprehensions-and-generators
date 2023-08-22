35b780191397191b70c305b56f466926f9f41c8b
Addonay <addonay.osoro@student.moringaschool.com>
2023-08-22 18:23:30 +0300

    solved the lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+def return_evens(sequence):
+    return [x for x in sequence if x % 2 == 0]
+print(return_evens([0, 1, 3, 5, 7, 8, 9]))
 
-def make_exclamation(sentence_list):
-    pass
+
+def make_exclamation(sentences):
+    return [s + "!" for s in sentences]
+print(make_exclamation(["Hello", "I'm doing great", "Python is fun"]))
